98fe80dbe74b495f8d5fea5e5a9b39d7d7d7333b
wxz <876688461@qq.com>
2019-03-06 15:15:18 +0800

    modify the use_ckpt

--- cleverhans_tutorials/using_ckpt.py
+++ cleverhans_tutorials/using_ckpt.py
@@ -2,9 +2,8 @@ import tensorflow as tf
 import sys
 
 model_path = sys.path[0] + '/simple_mnist.ckpt'
-saver = tf.train.Saver()
 
 
 with tf.Session() as sess:
-    load_path = saver.restore(sess, model_path)
-    print ("[+] Model restored from %s" % load_path)
+    saver = tf.train.import_meta_graph('simple_cifar10.ckpt.meta')
+    saver.restore(sess, tf.train.latest_checkpoint("."))
