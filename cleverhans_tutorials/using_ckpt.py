74bc296ccf21c46982bb6e03f158e6e8083fad03
wxz <876688461@qq.com>
2019-03-06 21:10:28 +0800

    modify the use_ckpt

--- cleverhans_tutorials/using_ckpt.py
+++ cleverhans_tutorials/using_ckpt.py
@@ -34,7 +34,7 @@ def do_eval(preds, x_set, y_set, report_key, is_adv=None):
 with tf.Session() as sess:
     model = ModelAllConvolutional('model2', 10, 64,
                                  input_shape=[32, 32, 3])
-    # restore_model_evaluate(sess)
+    restore_model_evaluate(sess)
 
     data = CIFAR10(train_start=0, train_end=60000,
                    test_start=0, test_end=10000)
@@ -56,5 +56,7 @@ with tf.Session() as sess:
                                           nchannels))
     y = tf.placeholder(tf.float32, shape=(None, nb_classes))
 
-    preds = model.get_logits(x)
+    graph = tf.get_default_graph()
+    model1 = graph.get_tensor_by_name("model1")
+    preds = model1.get_logits(x)
     do_eval(preds, x_test, y_test, 'clean_train_clean_eval', False)
