6edd9c12da2306873da90d39b9abe086d5199d65
wxz <876688461@qq.com>
2019-03-06 23:20:01 +0800

    use graph.get_tensor_by_name function.

--- cleverhans_tutorials/using_ckpt.py
+++ cleverhans_tutorials/using_ckpt.py
@@ -55,6 +55,6 @@ with tf.Session() as sess:
     y = tf.placeholder(tf.float32, shape=(None, nb_classes))
 
     graph = tf.get_default_graph()
-    preds = graph.get_tensor_by_name("preds")
+    preds = graph.get_tensor_by_name("preds:0")
     # preds = model1.get_logits(x)
     # do_eval(preds, x_test, y_test, 'clean_train_clean_eval', False)
