9f4bab42c6df8b025dd57545f867bdef120ded40
wxz <876688461@qq.com>
2019-03-07 09:52:00 +0800

    ckpt

--- cleverhans_tutorials/using_ckpt.py
+++ cleverhans_tutorials/using_ckpt.py
@@ -58,6 +58,4 @@ with tf.Session() as sess:
 
     preds = model1.get_logits(x)
 
-    # preds = model1.get_logits(x)
-
-    # do_eval(preds, x_test, y_test, 'clean_train_clean_eval', False)
+    do_eval(preds, x_test, y_test, 'clean_train_clean_eval', False)
