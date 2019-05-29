ee7909df376aaada61640fe4c07e981cff69b641
wxz <876688461@qq.com>
2019-05-29 17:59:03 +0800

    add cmodel

--- cleverhans_tutorials/mymodel.py
+++ cleverhans_tutorials/mymodel.py
@@ -57,3 +57,45 @@ def fc_modelB(logits=False, input_ph=None, img_rows=28, img_cols=28,
   else:
     return model
 
+def fc_modelC(logits=False, input_ph=None, img_rows=28, img_cols=28,
+              channels=1, nb_filters=64, nb_classes=10):
+  """
+  Defines a CNN model using Keras sequential model
+  :param logits: If set to False, returns a Keras model, otherwise will also
+                  return logits tensor
+  :param input_ph: The TensorFlow tensor for the input
+                  (needed if returning logits)
+                  ("ph" stands for placeholder but it need not actually be a
+                  placeholder)
+  :param img_rows: number of row in the image
+  :param img_cols: number of columns in the image
+  :param channels: number of color channels (e.g., 1 for MNIST)
+  :param nb_filters: number of convolutional filters per layer
+  :param nb_classes: the number of output classes
+  :return:
+  """
+  model = Sequential()
+
+  # Define the layers successively (convolution layers are version dependent)
+  if keras.backend.image_dim_ordering() == 'th':
+    input_shape = (channels, img_rows, img_cols)
+  else:
+    input_shape = (img_rows, img_cols, channels)
+
+  model = keras.Sequential([
+      keras.layers.Conv2D(input_shape=(28, 28, 1),
+                          kernel_size=(3, 3), filters=32, activation='relu'),
+      keras.layers.Conv2D(kernel_size=(3, 3), filters=32, activation='relu'),
+      keras.layers.MaxPool2D(),
+
+      keras.layers.Conv2D(kernel_size=(3, 3), filters=32, activation='relu'),
+      keras.layers.Conv2D(kernel_size=(3, 3), filters=32, activation='relu'),
+      keras.layers.MaxPool2D(),
+
+      keras.layers.Flatten(),
+      keras.layers.Dense(200, activation='relu'),
+      keras.layers.Dropout(0.5),
+      keras.layers.Dense(200, activation='relu'),
+      keras.layers.Dense(10, activation='softmax')
+  ])
+  return model
