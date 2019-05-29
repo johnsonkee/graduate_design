5386eb5e5a768aca33eada945ba80095ac02709a
wxz <876688461@qq.com>
2019-05-29 20:30:41 +0800

    merge at

--- cleverhans_tutorials/jiaoben.sh
+++ cleverhans_tutorials/jiaoben.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 python3 mnist_keras_at.py --save_model True --model_type a --origin_method fgsm
-python3 mnist_keras_at.py --save_model True --model_type a --origin_method bin
+python3 mnist_keras_at.py --save_model True --model_type a --origin_method bim
 python3 mnist_keras_at.py --save_model True --model_type a --origin_method mifgsm
 
 python3 mnist_keras_at.py --save_model True --model_type b --origin_method fgsm
-python3 mnist_keras_at.py --save_model True --model_type b --origin_method bin
+python3 mnist_keras_at.py --save_model True --model_type b --origin_method bim
 python3 mnist_keras_at.py --save_model True --model_type b --origin_method mifgsm
 
 python3 mnist_keras_at.py --save_model True --model_type c --origin_method fgsm
-python3 mnist_keras_at.py --save_model True --model_type c --origin_method bin
+python3 mnist_keras_at.py --save_model True --model_type c --origin_method bim
 python3 mnist_keras_at.py --save_model True --model_type c --origin_method mifgsm
